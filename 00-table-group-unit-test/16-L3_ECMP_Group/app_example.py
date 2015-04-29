9519e6936ca634cfcce3975ca5c9096b123e1501
macauley_cheng <macauleycheng@gmail.com>
2015-04-29 02:33:27 -0700

    add app_exmpale.py

---
+++ 00-table-group-unit-test/16-L3_ECMP_Group/app_example.py
@@ -0,0 +1,89 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER , MAIN_DISPATCHER, HANDSHAKE_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib import mac
+from ryu import utils
+
+class app_example(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+	
+    def __init__(self, *args, **kwargs):
+        super( app_example, self).__init__(*args, **kwargs)
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures , CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto		
+        parser = datapath.ofproto_parser	
+		
+        self.clearAllFlowsAndGroups(datapath)
+
+        #create l2_interface_group, vlan 1 port 1
+        actions = [parser.OFPActionOutput(port=1), parser.OFPActionPopVlan()]
+        buckets = [parser.OFPBucket(weight=100, watch_port=0, watch_group=0, actions=actions)]
+        self.add_group(datapath, ofproto.OFPGT_INDIRECT, 0x00020001, buckets)
+
+        #create l3_unicast_group, type is indirect, vlan_vid shall be l2_interface_group vid
+        actions = [parser.OFPActionSetField(vlan_vid=2),
+                   parser.OFPActionSetField(eth_src="00:00:00:22:44:66"), parser.OFPActionGroup(0x00020001)]
+        buckets = [parser.OFPBucket(weight=100, watch_port=0, watch_group=0, actions=actions)]
+        self.add_group(datapath, ofproto.OFPGT_INDIRECT, 0x20000001, buckets)
+
+        #create ecmp_group, type is select
+        actions = [parser.OFPActionGroup(0x20000001)]
+        buckets = [parser.OFPBucket(weight=100, watch_port=0, watch_group=0, actions=actions)]
+        self.add_group(datapath, ofproto.OFPGT_SELECT, 0x70000001, buckets)
+
+		
+    @set_ev_cls(ofp_event.EventOFPPacketIn , MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+    @set_ev_cls(ofp_event.EventOFPErrorMsg, [HANDSHAKE_DISPATCHER, CONFIG_DISPATCHER, MAIN_DISPATCHER])
+    def error_msg_handler(self, ev):
+        msg = ev.msg
+        print "OFPErrorMsg received: type=0x%02x code=0x%02x 'message=%s'"%(msg.type, msg.code, utils.hex_array(msg.data))
+
+    def clearAllFlowsAndGroups(self, datapath):	
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser		
+        #clear all flows
+        match = parser.OFPMatch()
+        self.del_flow(datapath , ofproto.OFPTT_ALL, 0, match, inst=[])
+        #clear all groups
+        buckets = []
+        self.del_group(datapath, ofproto.OFPGT_INDIRECT, ofproto.OFPG_ALL, buckets)	
+
+    def add_group(self, datapath, type, group_id, buckets):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser		
+        req = parser.OFPGroupMod(datapath, ofproto.OFPGC_ADD,
+                                 type, group_id, buckets)
+        datapath.send_msg(req)		
+
+    def del_group(self, datapath, type, group_id, buckets):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser		
+        req = parser.OFPGroupMod(datapath, ofproto.OFPGC_DELETE,
+                                 type, group_id, buckets)
+        datapath.send_msg(req)	
+		
+    def add_flow(self, datapath , table_id, priority , match , inst):
+       ofproto = datapath.ofproto
+       parser = datapath.ofproto_parser
+       mod = parser.OFPFlowMod(datapath=datapath, table_id=table_id, priority=priority ,
+                               match=match, instructions=inst)
+       datapath.send_msg(mod)
+
+    def del_flow(self, datapath , table_id, priority , match , inst):
+       ofproto = datapath.ofproto
+       parser = datapath.ofproto_parser
+       mod = parser.OFPFlowMod(datapath=datapath, command=ofproto.OFPFC_DELETE, table_id=table_id, priority=priority,
+	                           out_port=ofproto.OFPP_ANY, out_group=ofproto.OFPG_ANY,
+                               match=match , instructions=inst)
+       datapath.send_msg(mod)
