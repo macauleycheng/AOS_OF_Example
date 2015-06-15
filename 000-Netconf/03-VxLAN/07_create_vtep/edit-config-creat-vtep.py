4a76524d460c65b2b5b0ee28f3b0516f39cc8cc2
macauley_cheng <macauley_cheng@accton.com.tw>
2015-06-14 19:18:48 -0700

    correct content

--- 000-Netconf/03-VxLAN/07_create_vtep/edit-config-creat-vtep.py
+++ 000-Netconf/03-VxLAN/07_create_vtep/edit-config-creat-vtep.py
@@ -3,20 +3,44 @@ import ncclient
 import xml.etree.ElementTree as ET
 
 host = "192.168.1.1"
-username="root"
-password="root"
+username="netconfuser"
+password="netconfuser"
+
+config_vni_xml="""
+  <config>
+      <of11-config:capable-switch xmlns:of11-config="urn:onf:of111:config:yang">
+        <ofdpa10:vni xmlns:ofdpa10="urn:bcm:ofdpa10:accton01">
+          <ofdpa10:id>101</ofdpa10:id>
+        </ofdpa10:vni>
+      </of11-config:capable-switch>
+  </config>
+  """
+config_nexthop_xml="""
+  <config>
+      <of11-config:capable-switch xmlns:of11-config="urn:onf:of111:config:yang">
+        <ofdpa10:next-hop xmlns:ofdpa10="urn:bcm:ofdpa10:accton01">
+          <ofdpa10:id>1</ofdpa10:id>
+          <ofdpa10:dest-mac>00:00:00:11:22:33</ofdpa10:dest-mac>
+          <ofdpa10:phy-port>1</ofdpa10:phy-port>
+          <ofdpa10:vid>2</ofdpa10:vid>
+        </ofdpa10:next-hop>
+      </of11-config:capable-switch>
+  </config>
+  """
 
 #due to ofconfig design problem, it need fill port feature
 #but we won't use it currently. 
- 
+#It need change below two value according to your SWITCH CPU MAC
+#<id>00:00:70:72:cf:dc:9d:b2</id>
+#<datapath-id>00:00:70:72:cf:dc:9d:b2</datapath-id> 
+  
 config_xml="""
   <config>
     <capable-switch xmlns="urn:onf:of111:config:yang">
       <id>capable-switch-1</id>
-      <ofdpa10:udp-dest-port xmlns:ofdpa10="urn:bcm:ofdpa10:accton01">6633</ofdpa10:udp-dest-port>
       <resources>
        <port>
-         <resource-id>100000</resource-id>     
+         <resource-id>10006</resource-id>     
            <features>
              <current>
                <rate>10Gb</rate>
@@ -43,33 +67,59 @@ config_xml="""
              </advertised-peer>        
            </features>
                 <ofdpa10:vtep xmlns:ofdpa10="urn:bcm:ofdpa10:accton01">
-                  <ofdpa10:src-ip>10.1.2.3</ofdpa10:src-ip>
-                  <ofdpa10:dest-ip>10.1.1.1</ofdpa10:dest-ip>
-                  <ofdpa10:udp-src-port>6633</ofdpa10:udp-src-port>
-                  <ofdpa10:vni>10</ofdpa10:vni>
-                  <ofdpa10:nexthop-id>10</ofdpa10:nexthop-id>
-                  <ofdpa10:ecmp-id>10</ofdpa10:ecmp-id>
-                  <ofdpa10:ttl>25</ofdpa10:ttl>
+                  <ofdpa10:src-ip>192.168.2.1</ofdpa10:src-ip>
+                  <ofdpa10:dest-ip>192.168.2.2</ofdpa10:dest-ip>
+                  <ofdpa10:udp-src-port>65535</ofdpa10:udp-src-port>
+                  <ofdpa10:vni>101</ofdpa10:vni>
+                  <ofdpa10:nexthop-id>1</ofdpa10:nexthop-id>
+                  <ofdpa10:ttl>255</ofdpa10:ttl>
                 </ofdpa10:vtep>
        </port> 
       </resources>
       <logical-switches>
           <switch>
-            <id>00:00:70:72:cf:b5:ea:88</id>
-            <datapath-id>00:00:70:72:cf:b5:ea:88</datapath-id>
+            <id>00:00:70:72:cf:dc:9e:da</id>
+            <datapath-id>00:00:70:72:cf:dc:9e:da</datapath-id>
             <resources>
-              <port>100000</port>
+              <port>10006</port>
             </resources>
           </switch>
-      </logical-switches>
+      </logical-switches>      
     </capable-switch>
-
-  </config>  """
+  </config>
+  """
   
 with manager.connect_ssh(host=host, port=830, username=username, password=password, hostkey_verify=False ) as m:
-    print m.edit_config(target='running', 
-                      config=config_xml, 
-                      default_operation='merge', 
-                      error_option='stop-on-error')
+    print m.get_config(source='running').data_xml
+    
+    print "Create VNI"
+    try:
+        print m.edit_config(target='running', 
+                           config=config_vni_xml, 
+                           default_operation='merge', 
+                           error_option='stop-on-error')
+    except Exception as rpc_error:
+        print rpc_error
+        exit();
+
+    print "Create NextHop"
+    try:
+        print m.edit_config(target='running', 
+                           config=config_nexthop_xml, 
+                           default_operation='merge', 
+                           error_option='stop-on-error')
+    except Exception as rpc_error:
+        print rpc_error
+        exit();
+        
+    print "Create VTEP"
+    try:
+        print m.edit_config(target='running', 
+                           config=config_xml, 
+                           default_operation='merge', 
+                           error_option='stop-on-error')
+    except Exception as rpc_error:
+        print rpc_error
+        exit();
 
     print m.get_config(source='running').data_xml
