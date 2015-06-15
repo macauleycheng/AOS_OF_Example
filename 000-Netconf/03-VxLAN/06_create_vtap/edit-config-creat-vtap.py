f0fabfdbceebeb229df50bcf00b8879e1f30c2be
macauley_cheng <macauley_cheng@accton.com.tw>
2015-06-14 19:18:54 -0700

    correct content

--- 000-Netconf/03-VxLAN/06_create_vtap/edit-config-creat-vtap.py
+++ 000-Netconf/03-VxLAN/06_create_vtap/edit-config-creat-vtap.py
@@ -3,11 +3,21 @@ import ncclient
 import xml.etree.ElementTree as ET
 
 host = "192.168.1.1"
-username="root"
-password="root"
+username="netconfuser"
+password="netconfuser"
 
 #due to ofconfig design problem, it need fill port feature 
 #but we won't use it currently.
+
+config_vni_xml="""
+  <config>
+      <of11-config:capable-switch xmlns:of11-config="urn:onf:of111:config:yang">
+        <ofdpa10:vni xmlns:ofdpa10="urn:bcm:ofdpa10:accton01">
+          <ofdpa10:id>10</ofdpa10:id>
+        </ofdpa10:vni>
+      </of11-config:capable-switch>
+  </config>
+  """
 
 config_xml="""
   <config>
@@ -15,7 +25,7 @@ config_xml="""
       <id>capable-switch-1</id>
       <resources>
        <port>
-         <resource-id>65538</resource-id>     
+         <resource-id>10005</resource-id>     
            <features>
              <current>
                <rate>10Gb</rate>
@@ -42,7 +52,7 @@ config_xml="""
              </advertised-peer>        
            </features>
            <ofdpa10:vtap xmlns:ofdpa10="urn:bcm:ofdpa10:accton01">
-             <ofdpa10:phy-port>6633</ofdpa10:phy-port>
+             <ofdpa10:phy-port>1</ofdpa10:phy-port>
              <ofdpa10:vid>4093</ofdpa10:vid>
              <ofdpa10:vni>10</ofdpa10:vni>
            </ofdpa10:vtap>
@@ -50,19 +60,23 @@ config_xml="""
       </resources>
       <logical-switches>
           <switch>
-            <id>00:00:70:72:cf:b5:ea:88</id>
-            <datapath-id>00:00:70:72:cf:b5:ea:88</datapath-id>
+            <id>00:00:70:72:cf:dc:9e:da</id>
+            <datapath-id>00:00:70:72:cf:dc:9e:da</datapath-id>
             <resources>
-              <port>65538</port>
+              <port>10005</port>
             </resources>
           </switch>
       </logical-switches>
     </capable-switch>
-
   </config>
   """
   
 with manager.connect_ssh(host=host, port=830, username=username, password=password, hostkey_verify=False ) as m:
+    print m.edit_config(target='running', 
+                      config=config_vni_xml, 
+                      default_operation='merge', 
+                      error_option='stop-on-error')
+                      
     print m.edit_config(target='running', 
                       config=config_xml, 
                       default_operation='merge', 
