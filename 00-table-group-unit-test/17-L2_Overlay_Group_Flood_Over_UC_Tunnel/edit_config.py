6fc4f719b1b0cd8580ea895ef463d603873b1340
macauley_cheng <macauleycheng@gmail.com>
2015-06-16 03:14:30 -0700

    add case

---
+++ 00-table-group-unit-test/17-L2_Overlay_Group_Flood_Over_UC_Tunnel/edit_config.py
@@ -0,0 +1,228 @@
+import pkg_resources
+pkg_resources.require("ncclient==0.4.3")
+from ncclient import manager
+import ncclient
+
+
+#due to ofconfig design problem, it need fill port feature 
+#but we won't use it currently.
+
+#of-agent nexthop 2 destination user-input-dst-mac ethernet 1/2 vid 2
+config_nexthop_ucast_xml="""
+  <config>
+      <of11-config:capable-switch xmlns:of11-config="urn:onf:of111:config:yang">
+        <ofdpa10:next-hop xmlns:ofdpa10="urn:bcm:ofdpa10:accton01">
+          <ofdpa10:id>2</ofdpa10:id>
+          <ofdpa10:dest-mac>user-input-dst-mac</ofdpa10:dest-mac>
+          <ofdpa10:phy-port>2</ofdpa10:phy-port>
+          <ofdpa10:vid>2</ofdpa10:vid>
+        </ofdpa10:next-hop>
+      </of11-config:capable-switch>
+  </config>
+  """
+  
+#of-agent nexthop 20 destination 01-00-5e-01-01-01 ethernet 1/2 vid 2
+config_nexthop_mcast_xml="""
+  <config>
+      <of11-config:capable-switch xmlns:of11-config="urn:onf:of111:config:yang">
+        <ofdpa10:next-hop xmlns:ofdpa10="urn:bcm:ofdpa10:accton01">
+          <ofdpa10:id>20</ofdpa10:id>
+          <ofdpa10:dest-mac>01:00:5E:01:01:01</ofdpa10:dest-mac>
+          <ofdpa10:phy-port>2</ofdpa10:phy-port>
+          <ofdpa10:vid>2</ofdpa10:vid>
+        </ofdpa10:next-hop>
+      </of11-config:capable-switch>
+  </config>
+  """
+
+#of-agent vni 10
+config_vni_xml="""
+  <config>
+      <of11-config:capable-switch xmlns:of11-config="urn:onf:of111:config:yang">
+        <ofdpa10:vni xmlns:ofdpa10="urn:bcm:ofdpa10:accton01">
+          <ofdpa10:id>10</ofdpa10:id>
+          <ofdpa10:vni-multicast-group>224.1.1.1</ofdpa10:vni-multicast-group>
+          <ofdpa10:multicast-group-nexthop-id>20</ofdpa10:multicast-group-nexthop-id>
+        </ofdpa10:vni>
+      </of11-config:capable-switch>
+  </config>
+  """
+  
+  
+#of-agent vtap 10001 ethernet 1/1 vid 1
+#of-agent vtp 10001 vni 10
+config_vtap_xml="""
+  <config>
+    <capable-switch xmlns="urn:onf:of111:config:yang">
+      <id>capable-switch-1</id>
+      <resources>
+       <port>
+         <resource-id>10001</resource-id>     
+           <features>
+             <current>
+               <rate>10Gb</rate>
+               <medium>fiber</medium>
+               <pause>symmetric</pause>      
+             </current>
+             <advertised>
+               <rate>10Gb</rate>
+               <rate>100Gb</rate>
+               <medium>fiber</medium>
+               <pause>symmetric</pause>
+             </advertised>    
+             <supported>
+               <rate>10Gb</rate>
+               <rate>100Gb</rate>
+               <medium>fiber</medium>
+               <pause>symmetric</pause>
+             </supported> 
+             <advertised-peer>
+               <rate>10Gb</rate>
+               <rate>100Gb</rate>
+               <medium>fiber</medium>
+               <pause>symmetric</pause>
+             </advertised-peer>        
+           </features>
+           <ofdpa10:vtap xmlns:ofdpa10="urn:bcm:ofdpa10:accton01">
+             <ofdpa10:phy-port>1</ofdpa10:phy-port>
+             <ofdpa10:vid>1</ofdpa10:vid>
+             <ofdpa10:vni>10</ofdpa10:vni>
+           </ofdpa10:vtap>
+       </port> 
+      </resources>
+      <logical-switches>
+          <switch>
+            <id>user-input-switch-cpu-mac</id>
+            <datapath-id>user-input-switch-cpu-mac</datapath-id>
+            <resources>
+              <port>10001</port>
+            </resources>
+          </switch>
+      </logical-switches>
+    </capable-switch>
+  </config>
+  """
+  
+#of-agent vtep 10002 source user-input-src-ip destination user-input-dst-ip udp-source-port 6633 nexthop 2 ttl 25
+config_vtep_xml="""
+  <config>
+    <capable-switch xmlns="urn:onf:of111:config:yang">
+      <id>capable-switch-1</id>
+      <ofdpa10:udp-dest-port xmlns:ofdpa10="urn:bcm:ofdpa10:accton01">6633</ofdpa10:udp-dest-port>
+      <resources>
+       <port>
+         <resource-id>10002</resource-id>     
+           <features>
+             <current>
+               <rate>10Gb</rate>
+               <medium>fiber</medium>
+               <pause>symmetric</pause>      
+             </current>
+             <advertised>
+               <rate>10Gb</rate>
+               <rate>100Gb</rate>
+               <medium>fiber</medium>
+               <pause>symmetric</pause>
+             </advertised>    
+             <supported>
+               <rate>10Gb</rate>
+               <rate>100Gb</rate>
+               <medium>fiber</medium>
+               <pause>symmetric</pause>
+             </supported> 
+             <advertised-peer>
+               <rate>10Gb</rate>
+               <rate>100Gb</rate>
+               <medium>fiber</medium>
+               <pause>symmetric</pause>
+             </advertised-peer>        
+           </features>
+                <ofdpa10:vtep xmlns:ofdpa10="urn:bcm:ofdpa10:accton01">
+                  <ofdpa10:src-ip>user-input-src-ip</ofdpa10:src-ip>
+                  <ofdpa10:dest-ip>user-input-dst-ip</ofdpa10:dest-ip>
+                  <ofdpa10:udp-src-port>6633</ofdpa10:udp-src-port>
+                  <ofdpa10:vni>10</ofdpa10:vni>
+                  <ofdpa10:nexthop-id>2</ofdpa10:nexthop-id>
+                  <ofdpa10:ttl>25</ofdpa10:ttl>
+                </ofdpa10:vtep>
+       </port> 
+      </resources>
+      <logical-switches>
+          <switch>
+            <id>user-input-switch-cpu-mac</id>
+            <datapath-id>user-input-switch-cpu-mac</datapath-id>
+            <resources>
+              <port>10002</port>
+            </resources>
+          </switch>
+      </logical-switches>
+    </capable-switch>
+  </config>  
+  """
+
+def replace_vtep_vtap_nexthop(sip, dip, smac, dmac):
+    global nexthop_ucast_xml
+    nexthop_ucast_xml=config_nexthop_ucast_xml.replace("user-input-dst-mac", dmac)
+	
+    global vtep_xml
+    vtep_xml=config_vtep_xml.replace("user-input-switch-cpu-mac", "00:00:"+smac)
+    vtep_xml=vtep_xml.replace("user-input-src-ip", sip)
+    vtep_xml=vtep_xml.replace("user-input-dst-ip", dip)
+	
+    global vtap_xml
+    vtap_xml=config_vtap_xml.replace("user-input-switch-cpu-mac","00:00:"+smac)
+    
+
+def send_edit_config(host_ip, username, password):
+    with manager.connect_ssh(host=host_ip, port=830, username=username, password=password, hostkey_verify=False ) as m:
+        try:
+            m.edit_config(target='running', 
+                          config=nexthop_ucast_xml, 
+                          default_operation='merge', 
+                          error_option='stop-on-error')
+        except Exception as e:
+            print "Fail to edit-config config_nexthop_ucast_xml"
+            return -1
+
+        try:
+            m.edit_config(target='running', 
+                          config=config_nexthop_mcast_xml, 
+                          default_operation='merge', 
+                          error_option='stop-on-error')
+        except Exception as e:
+            print "Fail to edit-config config_nexthop_mcast_xml"
+            return -1
+
+        try:
+            m.edit_config(target='running', 
+                          config=config_vni_xml, 
+                          default_operation='merge', 
+                          error_option='stop-on-error')
+        except Exception as e:
+            print "Fail to edit-config config_vni_xml"	
+            return -1
+        		
+        try:
+            m.edit_config(target='running', 
+                          config=vtep_xml, 
+                          default_operation='merge', 
+                          error_option='stop-on-error')
+        except Exception as e:
+            print "Fail to edit-config vtep_xml"	
+            return -1
+
+        try:
+            m.edit_config(target='running', 
+                          config=vtap_xml, 
+                          default_operation='merge', 
+                          error_option='stop-on-error')
+        except Exception as e:
+            print "Fail to edit-config vtap_xml"	
+            return -1
+
+    	print m.get_config(source='running').data_xml
+    
+
+#replace_vtep_vtap_nexthop("10.1.1.1", "10.1.2.1", "70:72:cf:dc:9e:da", "70:72:cf:b5:ea:88")
+#send_edit_config("192.168.1.1", "netconfuser", "netconfuser")
+
---
+++ 00-table-group-unit-test/20-L2_OVerlay_Group_Mcast_Over_MC_Tunnel/app_example.py
@@ -0,0 +1,107 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER , MAIN_DISPATCHER, HANDSHAKE_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib import mac
+from ryu import utils
+import edit_config
+
+class app_example(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+	
+    def __init__(self, *args, **kwargs):
+        super( app_example, self).__init__(*args, **kwargs)
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures , CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto		
+        parser = datapath.ofproto_parser			
+        self.clearAllFlowsAndGroups(datapath)
+        
+        print "Set VxLAN config"
+        edit_config.replace_vtep_vtap_nexthop("10.1.1.1", "10.1.2.1", "70:72:cf:dc:9e:da", "70:72:cf:b5:ea:88")
+        edit_config.send_edit_config("192.168.1.1", "netconfuser", "netconfuser")
+
+        print "Start to add L2 overlay Flood Group"
+        #create L2_Overlay_Flood_Group
+        #group_id=([9:0}index, [11:10]subtgype, [27:12]TunnelId, [31:28]:8
+        #shall a VTEP phy_port same with "of-agent vni 10 multicast 224.1.1.1 nexthop 20"
+        #"of-agent nexthop 20 destination 01-00-5e-01-01-01 ethernet 1/2 vid 2"
+        #used phy_port.
+        #And currently we only provide one port ouput multicast packet
+        action_vtep = [parser.OFPActionOutput(port=0x10001)]
+        action_vtap = [parser.OFPActionOutput(port=0x10002)]
+        buckets = [parser.OFPBucket(weight=100, watch_port=0, watch_group=0, actions=action_vtep),
+                   parser.OFPBucket(weight=100, watch_port=0, watch_group=0, actions=action_vtap)]
+        self.add_group(datapath, ofproto.OFPGT_ALL, 0x8000ac01, buckets)
+        print "Finish adding L2 Overlay Flood Group"        
+		
+        print "Add Flow table 0, just add this flow"
+        inst = [parser.OFPInstructionGotoTable(50)]
+        match = parser.OFPMatch()
+        match.set_in_port(0x10000)
+        self.add_flow(datapath ,0 ,1, match , inst)   		
+        
+        print "add Bridge Flow"
+        #add bridge flow        
+        actions = [parser.OFPActionGroup(0x8000ac01)]        
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_WRITE_ACTIONS, actions),
+                parser.OFPInstructionGotoTable(60)]
+        match = parser.OFPMatch()
+        #on edit_config we use VNI 10
+        match.set_tunnel_id(10) 
+        match.set_dl_dst(mac.haddr_to_bin("01:00:5e:01:01:01"))        
+        self.add_flow(datapath ,50 ,1, match , inst)     
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn , MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+    @set_ev_cls(ofp_event.EventOFPErrorMsg, [HANDSHAKE_DISPATCHER, CONFIG_DISPATCHER, MAIN_DISPATCHER])
+    def error_msg_handler(self, ev):
+        msg = ev.msg
+        print "OFPErrorMsg received: type=0x%02x code=0x%02x 'message=%s'"%(msg.type, msg.code, utils.hex_array(msg.data))
+
+    def clearAllFlowsAndGroups(self, datapath):	
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser		
+        #clear all flows
+        match = parser.OFPMatch()
+        self.del_flow(datapath , ofproto.OFPTT_ALL, 0, match, inst=[])
+        #clear all groups
+        buckets = []
+        self.del_group(datapath, ofproto.OFPGT_INDIRECT, ofproto.OFPG_ALL, buckets)	
+
+    def add_group(self, datapath, type, group_id, buckets):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser		
+        req = parser.OFPGroupMod(datapath, ofproto.OFPGC_ADD,
+                                 type, group_id, buckets)
+        datapath.send_msg(req)		
+
+    def del_group(self, datapath, type, group_id, buckets):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser		
+        req = parser.OFPGroupMod(datapath, ofproto.OFPGC_DELETE,
+                                 type, group_id, buckets)
+        datapath.send_msg(req)	
+		
+    def add_flow(self, datapath , table_id, priority , match , inst):
+       ofproto = datapath.ofproto
+       parser = datapath.ofproto_parser
+       mod = parser.OFPFlowMod(datapath=datapath, table_id=table_id, priority=priority ,
+                               match=match, instructions=inst)
+       datapath.send_msg(mod)
+
+    def del_flow(self, datapath , table_id, priority , match , inst):
+       ofproto = datapath.ofproto
+       parser = datapath.ofproto_parser
+       mod = parser.OFPFlowMod(datapath=datapath, command=ofproto.OFPFC_DELETE, table_id=table_id, priority=priority,
+	                           out_port=ofproto.OFPP_ANY, out_group=ofproto.OFPG_ANY,
+                               match=match , instructions=inst)
+       datapath.send_msg(mod)
