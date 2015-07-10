df0cf6e437c07a359e20098a936365c5252be5f3
macauley <macauleycheng@google.com>
2015-07-10 17:45:46 +0800

    correct delte namespace

--- 000-Netconf/get-config-running.py
+++ 000-Netconf/get-config-running.py
@@ -3,8 +3,8 @@ import ncclient
 import xml.etree.ElementTree as ET
 
 host = "192.168.1.1"
-username="root"
-password="root"
+username="netconfuser"
+password="netconfuser"
 
            
 with manager.connect_ssh(host=host, port=830, username=username, password=password, hostkey_verify=False ) as m:
